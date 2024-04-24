4e5602a6bcfa00db2b525449108383f189302fb6
Ian Ku <iankuyisien@gmail.com>
2024-04-24 01:11:31 -0400

    add gallery and about page place holder

---
+++ app/about/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import React from 'react';
+import { HeaderMenu } from '@/components/Home/HeaderMenu';
+import { FooterLinks } from '@/components/Home/FooterBar';
+
+export default function page() {
+  return (
+    <>
+        <HeaderMenu />
+        <div>page</div>
+        <FooterLinks />
+    </>
+  )
+}

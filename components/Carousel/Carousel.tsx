3b4631012977763cd8445a2a62e39745911a0c16
Ian Ku <iankuyisien@gmail.com>
2024-06-12 02:23:51 -0400

    improved carousel layout

--- components/Carousel/Carousel.tsx
+++ components/Carousel/Carousel.tsx
@@ -1,6 +1,6 @@
 import { Carousel } from '@mantine/carousel';
 import { useMediaQuery } from '@mantine/hooks';
-import { Paper, Modal, Image, useMantineTheme, rem } from '@mantine/core';
+import { Paper, Modal, Center, Image, useMantineTheme, rem } from '@mantine/core';
 import classes from './CardsCarousel.module.css';
 import '@mantine/carousel/styles.css';
 import { useState } from 'react';
@@ -13,7 +13,7 @@ function Card({ url }: CardProps) {
         <Paper
             shadow="md"
             p="xl"
-            radius="sm"
+            radius="md"
             style={{ backgroundImage: `url(${url})` }}
             className={classes.card}
             withBorder
@@ -34,15 +34,14 @@ export function CardsCarousel({ carouselCard }: { carouselCard: carouselImageObj
     ));
 
     return (
-        <>
+        <Center>
             <Carousel
                 slideGap={{ base: 'xs', sm: 'xl', xl: 'sm' }}
                 align="start"
                 slidesToScroll={mobile ? 1 : 1}
                 controlsOffset="md"
                 controlSize={30}
-                mx={{}}
-                style={{ marginBottom: rem(50) }}
+                style={{ border: 0, width: '70%', boxShadow: '0 0 20px rgba(0, 0, 0, 0.75)' }}
                 loop
             >
                 {slides}
@@ -51,6 +50,6 @@ export function CardsCarousel({ carouselCard }: { carouselCard: carouselImageObj
             <Modal opened={modalOpened} onClose={() => setModalOpened(false)} size="xl" withCloseButton={false} centered>
                 <Image src={selectedImage} alt="Selected" style={{ width: '100%' }} />
             </Modal>
-        </>
+        </Center>
     );
 }
--- components/Products/ProductDescription.tsx
+++ components/Products/ProductDescription.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { Center, Grid, Image, Title } from '@mantine/core';
-import { getUrlResult } from '@/src/helper';
+import { Center, Grid, Card, Title, Text } from '@mantine/core';
 import { productRawObject, productCard, carouselImageObject } from '@/src/types';
 import { CardsCarousel } from '../Carousel/Carousel';
 import { fetchURL } from '@/src/helper';
+import { rem } from '@mantine/core';
 
 export default function ProductDescription({ object }: { object: productRawObject }) {
 
@@ -28,17 +28,19 @@ export default function ProductDescription({ object }: { object: productRawObjec
 
   return (
     <>
-      <Center h={100} bg="var(--mantine-color-gray-light)">
-        <Title order={2}>
+      <Center h={120} bg="var(--mantine-color-gray-light)">
+        <Title order={1}>
           {object.title}
         </Title>
       </Center>
-      <Grid justify="center" align="center" py="1rem">
+      <Grid justify="center" py="1rem" style={{ margin: rem(30), }}>
         <Grid.Col span={6}>
-          {object.description}
+          <Center style={{ paddingLeft: '10%' }}>
+            <Text>{object.description}</Text>
+          </Center>
         </Grid.Col>
         <Grid.Col span={6}>
-          <CardsCarousel carouselCard={productObject}/>
+          <CardsCarousel carouselCard={productObject} />
         </Grid.Col>
       </Grid>
 
